414df31e1027fab8f4c7df38f015a9f65bb907ca
Efosa <okoobohefosa@gmail.com>
2023-06-19 17:10:50 +0100

    implement add and delete todo functionality

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.add_todo, name='add_todo'),
+    path('remove/<int:todo_id>', views.remove_add, name='remove_todo'),
+    
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,20 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+
+todo_database = [
+    {
+        'id':1,
+        'todo_item': 'First todo',
+    },
+    {
+        'id':2,
+        'todo_item': 'Second todo',
+    },
+    {
+        'id':3,
+        'todo_item': 'Third todo',
+    },
+]
 
 def index(request):
-    todo_items = [
-        'First task',
-        'Second task',
-        'Third task',
-        'Fourth task'
-    ]
+
     context = {
-        'todos': todo_items
+        'todos': todo_database
     }
 
     return render(request, template_name='todo/index.html', context=context)
 
 
 def add_todo(request):
+    if request.method == 'POST':
+        todo_item = request.POST.get('todo')
+
+        todo_id = None
+        if len(todo_database) == 0:
+            todo_id = 0
+        else:
+            todo_id = todo_database[-1].get('id') + 1
+            
+        todo_database.append({
+            'id': todo_id,
+            'todo_item': todo_item
+        })
 
     return render(request, template_name='todo/add.html')
+
+
+
+def remove_add(request, todo_id):
+    for todo in todo_database:
+        if todo.get('id') == todo_id:
+            todo_database.remove(todo)
+
+    return redirect('index')
